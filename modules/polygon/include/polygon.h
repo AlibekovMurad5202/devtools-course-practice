e3449baed5bee0597dcb22686d496aad1590743e
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2021-04-26 06:28:20 +0300

    added lab2 (polygon)

---
+++ modules/polygon/include/polygon.h
@@ -0,0 +1,18 @@
+// Copyright 2021 Alibekov Murad
+
+#ifndef MODULES_POLYGON_INCLUDE_POLYGON_H_
+#define MODULES_POLYGON_INCLUDE_POLYGON_H_
+
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+
+using Point2D = std::pair<double, double>;
+using Points2D = std::vector<Point2D>;
+
+namespace Polygon {
+    double PolygonArea(const Points2D& polygon);
+}
+
+#endif  // MODULES_POLYGON_INCLUDE_POLYGON_H_
